19ef82e35ee2334e44d421bdc9730651e00fcd71
MurphyMarkW <murphy.mark.w+github@gmail.com>
2014-08-21 17:54:32 -0500

    First pass implementation of unalignment script.

--- python/bam/unalign.py
+++ python/bam/unalign.py
@@ -6,15 +6,45 @@ import argparse
 
 import pysam
 
-def unalign(ifs,ofs):
+def unalign(ifs,ofs,tags=None):
   '''
   Convert aligned bam IFS to unaligned bam OFS.
   '''
 
+  # Generate a new read that we'll use as the base.
+  unaligned = pysam.AlignedRead()
+
+  # Prepare the tag set to keep.
+  tags = tags and set(tags) or set([])
+
   for read in ifs:
-    # TODO delete alignment fields
-    unaligned = pysam.AlignedRead()
-    unaligned.seq = read.seq
+    # Copy the query name of the read.
+    unaligned.qname = read.qname
+
+    # Copy the sequence and the quality string.
+    unaligned.seq   = read.seq
+    unaligned.qual  = read.qual
+
+    # Copy reference to paired sequence, if exists.
+    unaligned.rnext = read.rnext
+
+    # Set target reference and position to unknown.
+    unaligned.tid = -1
+    unaligned.pos = -1
+    unaligned.rnext = -1
+    unaligned.pnext = -1
+
+    # Copy any applicable flags.
+    if read.is_paired: unaligned.flag = 0x1   | unaligned.flag
+    if read.is_read1:  unaligned.flag = 0x40  | unaligned.flag
+    else:              unaligned.flag = 0x80  | unaligned.flag
+
+    # Copy tags specified by the user.
+    unaligned.tags = []
+    for tag in read.tags:
+      if tag[0] in tags: unaligned.tags += [tag]
+
+    # Write the new unaligned copy.
     ofs.write(unaligned)
 
 def main(args):
@@ -34,7 +64,8 @@ def main(args):
   if 'PG' in header: del header['PG']
 
   ofs = pysam.Samfile(args.out,'wb',header = header)
-  unalign(ifs,ofs)
+  unalign(ifs,ofs,args.tags)
+  ofs.close()
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser(description='Signpost Daemon')
@@ -45,6 +76,8 @@ if __name__ == '__main__':
                       help='Set logging level to DEBUG.')
   parser.add_argument('-o','--out',dest='out',default=sys.stdout,
                       help='File for output. [STDOUT]')
+  parser.add_argument('-t','--tag',dest='tags',action='append',default=[],
+                      help='Tags to keep.')
 
   # Positional arguments.
   parser.add_argument('bam',help='Bam file to unalign.')
